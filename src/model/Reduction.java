526dd76c13168a970c54a38d8057e8dc51b2914e
Marieallemandou <marie.allemandou@edu.ece.fr>
2025-04-24 15:06:33 +0200

    ajout de la fonction reduction et dans le dao et dans la fonction principale, correction d'erreurs mineures

--- src/model/Reduction.java
+++ src/model/Reduction.java
@@ -1,22 +1,31 @@
-package model;
+package Model;
+
+import java.time.LocalDateTime;
 
 public class Reduction {
-    private int id;
-private int pourcentage;
-private String concerne;
-private int idAttraction;
 
-public Reduction(int id, int pourcentage, String concerne, int idAttraction) {
-this.id = id;
-this.pourcentage = pourcentage;
-this.concerne = concerne;
-this.idAttraction = idAttraction;
+
+
+    private int idReduction;
+    private int pourcentage; // entre 0 et 100
+    private String concerne;
+    private int idAttraction;
+    private LocalDateTime dateReservation; // si tu veux inclure une date
+
+    // Constructeur
+    public Reduction(int idReduction, int pourcentage, String concerne, int idAttraction) {
+        if (pourcentage < 0 || pourcentage > 100) {
+            throw new IllegalArgumentException("Le pourcentage doit être entre 0 et 100.");
+        }
+        this.idReduction = idReduction;
+        this.pourcentage = pourcentage;
+        this.concerne = concerne;
+        this.idAttraction = idAttraction;
+    }
+
+    // Getters
+    public int getIdReduction() {return idReduction;}
+    public int getPourcentage() {return pourcentage;}
+    public String getConcerne() {return concerne;}
+    public int getIdAttraction() { return idAttraction;}
 }
-
-@Override
-public String toString() {
-return "Réduction " + pourcentage + "% pour : " + concerne + ", Attraction ID : " + idAttraction;
-}
-}
-
-
