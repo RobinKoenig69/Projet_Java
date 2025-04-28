9614dbed4d438c924b874f225539ac596c608026
sosocld <sofia.caillaud@edu.ece.fr>
2025-04-28 12:46:14 +0200

    Mise à jour de Reservation.java : ajout gestion annulation et calcul prix

--- src/Model/Reservation.java
+++ src/Model/Reservation.java
@@ -8,20 +8,48 @@ public class Reservation {
     private float Prix;
     private int id_utilisateur;
     private int id_attraction;
+    private boolean estAnnulee;
 
-    //constructor
-    public Reservation(int id_reservation, LocalDateTime DATETIME, float Prix, int id_utilisateur, int id_attraction) {
+    // Constructor
+    public Reservation(int id_reservation, LocalDateTime DATETIME, float Prix, 
+                      int id_utilisateur, int id_attraction) {
         this.id_reservation = id_reservation;
         this.Date_reservation = DATETIME;
         this.Prix = Prix;
         this.id_utilisateur = id_utilisateur;
         this.id_attraction = id_attraction;
+        this.estAnnulee = false;
     }
 
-    //getters
-    public int getId_reservation() {return id_reservation;}
-    public LocalDateTime getDate_reservation() {return Date_reservation;}
-    public float getPrix() {return Prix;}
-    public int getId_utilisateur() {return id_utilisateur;}
-    public int getId_attraction() {return id_attraction;}
+    // Constructor simplifié 
+    public Reservation(LocalDateTime DATETIME, float Prix, 
+                      int id_utilisateur, int id_attraction) {
+        this(0, DATETIME, Prix, id_utilisateur, id_attraction);
+    }
+
+    // Getters
+    public int getId_reservation() { return id_reservation; }
+    public LocalDateTime getDate_reservation() { return Date_reservation; }
+    public float getPrix() { return Prix; }
+    public int getId_utilisateur() { return id_utilisateur; }
+    public int getId_attraction() { return id_attraction; }
+    public boolean isEstAnnulee() { return estAnnulee; }
+
+    // Setters
+    public void setEstAnnulee(boolean estAnnulee) { 
+        this.estAnnulee = estAnnulee; 
+    }
+
+    // Méthode métier
+    public void calculerPrixAvecReduction(float tauxReduction) {
+        if (tauxReduction > 0 && tauxReduction <= 1) {
+            this.Prix -= this.Prix * tauxReduction;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Réservation #%d - Attraction: %d - User: %d - Prix: %.2f€", 
+                           id_reservation, id_attraction, id_utilisateur, Prix);
+    }
 }
